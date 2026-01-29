a4a533d8a280e2ef6c71c8ace203df426ce8fb36
sunghwanism <msh2044@gmail.com>
2026-01-29 10:50:42 -0500

    [Update:wandbid] Add ENV setting for wandbid to reload wandb

--- script/sbatch.sh
+++ script/sbatch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --account=ctb-panch_gpu
-#SBATCH --job-name=DGI-emb_Training-
-#SBATCH --output=logs/DGI-emb_Training-.txt
+#SBATCH --job-name=DGI-emb_Training-1
+#SBATCH --output=logs/DGI-emb_Training-1.txt
 #SBATCH --nodes=1
 #SBATCH --gpus-per-node=1
 #SBATCH --cpus-per-task=8
@@ -19,27 +19,26 @@ srun nproc
 srun nvidia-smi
 
 source $SCRATCH/shmoon/envs/DRC/bin/activate
-srun which pip
 
 # For CUDA-Calucation
 export NX_CUGRAPH_AUTOCONFIG=True
 export NETWORKX_FALLBACK_TO_NX=True # Change GPU to CPU, if GPU is not available
 
 # NEED to set for wandb
-export WANDB_RUN_ID=""
-export WANDB_API_KEY=""
-export ENTITY_NAME=""
+export WANDB_RUN_NAME="Training-1_excpt_clossness"
+export WANDB_API_KEY=
+export ENTITY_NAME=
 
-### Save your wandb API key in your .bash_profile or replace $API_KEY with your actual API key.
-### Uncomment the line below and comment out "wandb offline" if running in online mode ###
+# [Important]
+# If you want to load_pretrained model, you need to set WANDB_RUN_ID
+# export WANDB_RUN_ID=""
 
-# wandb login $WANDB_API_KEY 
 srun python script/train.py \
      --config_path config/run.yaml \
      --wandb_key $WANDB_API_KEY \
      --entity_name $ENTITY_NAME \
      --project_name DeepResidueCluster \
-     --wandb_run_name $WANDB_RUN_ID \
+     --wandb_run_name $WANDB_RUN_NAME \
      --batch_size 512 \
      --num_workers 8 \
      --use_aug \
