96543db6681470a2e4d0725afbbe5960df42c4b8
sunghwanism <msh2044@gmail.com>
2026-01-29 10:42:03 -0500

    [Update:Sbatch Script] Change parameter and resource setting

--- script/sbatch.sh
+++ script/sbatch.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-#SBATCH --job-name=DGI-emb_Training-1
-#SBATCH --output=logs/DGI-emb_Training-1.txt
+#SBATCH --account=ctb-panch_gpu
+#SBATCH --job-name=DGI-emb_Training-
+#SBATCH --output=logs/DGI-emb_Training-.txt
 #SBATCH --nodes=1
 #SBATCH --gpus-per-node=1
-#SBATCH --time=01:00:00
-#SBATCH --partition=compute
-
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=8G
+#SBATCH --time=00:15:00
 
 # move to base PATH
 cd $SCRATCH/shmoon/DeepResidueCluster
@@ -18,15 +19,16 @@ srun nproc
 srun nvidia-smi
 
 source $SCRATCH/shmoon/envs/DRC/bin/activate
+srun which pip
 
 # For CUDA-Calucation
 export NX_CUGRAPH_AUTOCONFIG=True
 export NETWORKX_FALLBACK_TO_NX=True # Change GPU to CPU, if GPU is not available
 
 # NEED to set for wandb
-export WANDB_RUN_ID="Training-1"
-export WANDB_API_KEY=
-export ENTITY_NAME=
+export WANDB_RUN_ID=""
+export WANDB_API_KEY=""
+export ENTITY_NAME=""
 
 ### Save your wandb API key in your .bash_profile or replace $API_KEY with your actual API key.
 ### Uncomment the line below and comment out "wandb offline" if running in online mode ###
@@ -38,9 +40,8 @@ srun python script/train.py \
      --entity_name $ENTITY_NAME \
      --project_name DeepResidueCluster \
      --wandb_run_name $WANDB_RUN_ID \
-     --wandb_run_id $WANDB_RUN_ID \
-     --batch_size 32 \
-     --num_workers 16 \
-     --nowandb \
+     --batch_size 512 \
+     --num_workers 8 \
      --use_aug \
-     # --load_pretrained  \
+     # --load_pretrained \
+     # --wandb_run_id $WANDB_RUN_ID \
