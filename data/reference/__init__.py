4e94eb821487de9692a1b16a4f55063c2afe213d
sunghwanism <sunghwan.moon.ai@gmail.com>
2026-02-02 21:54:54 -0500

    [init Cancer Driver Generator] DriverGenerater for COSMIC & MutaGene

---
+++ data/reference/__init__.py
@@ -0,0 +1,10 @@
+from .residue_dictionary import residue1to3
+from .residue_dictionary import residue3to1
+from .addUniprot import protein_gene_dict
+
+
+__all__ = [
+    'residue1to3',
+    'residue3to1',
+    'protein_gene_dict'
+]
